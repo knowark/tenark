cca161ac232c5c0b328cd629eebba7872266cb04
eecheverry <eecheverry@nubark.com>
2019-04-25 10:16:58 -0500

    Implement tenant provider

--- tenark/provider/__init__.py
+++ tenark/provider/__init__.py
@@ -1,15 +1,2 @@
-from .tenant import Tenant
-
-
-tenant = None
-
-
-def get_tenant():
-    if not tenant:
-        tenant = Tenant()
-
-    return tenant
-
-
-def reset_tenant():
-    tenant = None
+from .provider import Provider
+from .standard_provider import StandardProvider
---
+++ tests/provider/test_provider.py
@@ -0,0 +1,39 @@
+from pytest import fixture, raises
+from tenark.models import Tenant
+from tenark.provider import (
+    Provider, StandardProvider)
+
+
+@fixture
+def provider() -> StandardProvider:
+    return StandardProvider()
+
+
+def test_provider_methods():
+    abstract_methods = Provider.__abstractmethods__
+
+    assert 'setup' in abstract_methods
+    assert 'get_tenant' in abstract_methods
+
+
+def test_standard_provider_instantiation(provider):
+    assert isinstance(provider, Provider)
+
+
+def test_standard_provider_setup(provider):
+    tenant = Tenant(name='Alpina')
+    assert provider.state.tenant is None
+    provider.setup(tenant)
+    assert provider.state.tenant == tenant
+
+
+def test_standard_provider_get_tenant(provider):
+    tenant = Tenant(name='Alpina')
+    assert provider.state.tenant is None
+    provider.setup(tenant)
+    assert provider.get_tenant() == tenant
+
+
+def test_standard_provider_get_tenant_not_set(provider):
+    with raises(ValueError):
+        assert provider.get_tenant()
